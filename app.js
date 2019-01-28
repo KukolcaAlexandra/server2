dfda72af7ab70ceee486db18fda5c27056fcdf64
Alexandra Kukolca <Alexandra_Kukolca1@epam.com>
2019-01-28 18:13:44 +0300

    feat: add local strategy

--- app.js
+++ app.js
@@ -8,28 +8,127 @@ const indexRouter = require('./routes/index');
 const newsRouter = require('./routes/news');
 const app = express();
 var mongoose = require('mongoose');
-
-/*import exppressSession from 'express-session';
-
-exppressSession({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-});*/
-
+var session = require('express-session');
 const User = require('./models/user');
+var flash = require('express-flash');
+
 var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy
   , FacebookStrategy = require('passport-facebook').Strategy;
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    //console.log(User);
+    User.findOne({ username: username }, function(err, user) {
+      if (err) {
+        console.log('err'); 
+        return done(err); 
+      }
+      if (!user) {
+        console.log('!user');
+        user = new User({ 
+          firstname: username
+        });
+        //user.facebookId = profile.id;
+        //user.firstname = profile.name.givenName;
+        //user.lastname = profile.name.familyName;
+        user.save((err, user) => {
+          if (err)
+            return done(err, false);
+          else 
+            return done(null, user);
+        })
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        console.log('incorrect password');
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+passport.use(new FacebookStrategy({
+  clientID: '612251399234373',
+  clientSecret: '4e2341920972fc5c5c0052e5f94fe66c',
+  callbackURL: "http://localhost:3000/auth/facebook/callback"
+},
+
+function(accessToken, refreshToken, profile, done) {
+  console.log('Facebook strategy');
+  console.log(profile);
+  //done(null, profile);
+  User.findOne({facebookId: profile.id}, (err, user) => {
+    if (err) {
+      return done(err, false);
+    }
+    if (!err && user !== null) {
+      return done(null, user);
+    } else {
+      user = new User({ 
+        username: profile.displayName
+      });
+      user.facebookId = profile.id;
+      user.firstname = profile.name.givenName;
+      user.lastname = profile.name.familyName;
+      user.save((err, user) => {
+        if (err)
+          return done(err, false);
+        else 
+          return done(null, user);
+      })
+    }
+  });
+  console.log('write in the data base');
+  
+  /*User.findOrCreate(..., function(err, user) {
+    if (err) { return done(err); }
+    done(null, user);
+  });*/
+}
+));
 
 
-app.use(require('express-session')({
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static('public'));
+//app.use(express.cookieParser());
+//app.use(express.bodyParser());
+//app.use(express.session({ secret: 'keyboard cat' }));
+app.use(session({
+  secret: "cats",
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+  });
+  
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+  /*User.findById(id, function(err, user) {
+    done(err, user);
+  });*/
+});
+
+/*app.use(require('express-session')({
   secret: 'keyboard cat',
   resave: true,
   saveUninitialized: true
 }));
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session());*/
 
 app.get('/auth/facebook', passport.authenticate('facebook'));
 
@@ -88,14 +187,7 @@ const logger = winston.createLogger({
   ]
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 /////////////////
 
@@ -113,54 +205,28 @@ app.use('/', indexRouter);
 app.use('/news', newsRouter);
 
 
-
-passport.use(new FacebookStrategy({
-  clientID: '612251399234373',
-  clientSecret: '4e2341920972fc5c5c0052e5f94fe66c',
-  callbackURL: "http://localhost:3000/auth/facebook/callback"
-},
-function(accessToken, refreshToken, profile, done) {
-  console.log('Facebook strategy');
-  console.log(profile);
-  //done(null, profile);
-  User.findOne({facebookId: profile.id}, (err, user) => {
-    if (err) {
-      return done(err, false);
+app.post('/login', //function(req, res, next) {
+  passport.authenticate('local', { successRedirect: '/news',
+                                   failureRedirect: '/error',
+                                   failureFlash: true })
+  /*passport.authenticate('local', function(err, user, info) {
+    if (err) { 
+      console.log('/login err');
+      return next(err); 
     }
-    if (!err && user !== null) {
-      return done(null, user);
-    } else {
-      user = new User({ 
-        username: profile.displayName
-      });
-      user.facebookId = profile.id;
-      user.firstname = profile.name.givenName;
-      user.lastname = profile.name.familyName;
-      user.save((err, user) => {
-        if (err)
-          return done(err, false);
-        else 
-          return done(null, user);
-      })
-    }
-  });
-  console.log('write in the data base');
-  
-  /*User.findOrCreate(..., function(err, user) {
-    if (err) { return done(err); }
-    done(null, user);
-  });*/
-}
-));
-
-passport.serializeUser(function(user, done) {
-done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-done(null, user);
-});
-
+    console.log('user');
+    console.log(user);
+    if (!user) {
+      console.log('/login !user'); 
+      return res.redirect('/login'); 
+    } 
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      console.log('/login res.logIn');
+        return res.redirect('/news');
+    });
+  })(req, res, next);*/
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
